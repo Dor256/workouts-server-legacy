e9565d9aa9921e33ae564456f34d30c704c64a3b
Dor256 <dorb@wix.com>
2020-11-28 20:40:31 +0200

    add workout router

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const enum CODE {
+export const enum STATUS {
   OK = 200,
   NOT_FOUND = 404,
   BAD_REQUEST = 400
--- src/routers/workout.ts
+++ src/routers/workout.ts
@@ -1,21 +1,21 @@
 import { Router } from 'express';
 import { IWorkout, repository } from '../../db';
-import { CODE } from '../consts';
+import { STATUS } from '../consts';
 
 export const workoutRouter = Router();
 
 workoutRouter.get('/', async (req, res) => {
   const workouts = await repository.getWorkouts();
-  res.status(CODE.OK).send(workouts);
+  res.status(STATUS.OK).send(workouts);
 });
 
 workoutRouter.post('/', async (req, res) => {
   const workout: IWorkout = req.body;
   if (!workout?.name) {
-    res.status(CODE.BAD_REQUEST);
+    res.status(STATUS.BAD_REQUEST);
   } else {
     await repository.addWorkout(workout);
-    res.status(CODE.OK);
+    res.status(STATUS.OK);
   }
   res.send();
 });
